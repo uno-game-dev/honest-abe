7b64dca794e31f216c588d108d691642dda17b8e
parkersprouse <psprouse@uno.edu>
2016-01-25 16:30:24 -0600

    Cleaned up the PlayerMotor script

--- Assets/Scripts/BaseCollision.cs
+++ Assets/Scripts/BaseCollision.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(Collider2D))]
 public abstract class BaseCollision : MonoBehaviour {
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -3,59 +3,30 @@
 [RequireComponent(typeof(PlayerCollision))]
 public class PlayerMotor : MonoBehaviour {
 
-    //public float maxJumpHeight = 4; // max units we can jump
-    //public float minJumpHeight = 1; // min units we can jump
-    //public float timeToJumpApex = .3f; // how long in seconds it takes for us to reach the top of our jump
     public float hMoveSpeed = 8, vMoveSpeed = 2;
     public float movementSmoothing = .115f;
-    [HideInInspector] public Vector3 velocity;
 
-    //private float gravity;
-    //private float maxJumpVelocity, minJumpVelocity;
-    //private float accelerationTimeAirborn = .2f;
-    //private float accelerationTimeGrounded = .05f;
+    private Vector3 velocity;
     private float velocityXSmoothing, velocityYSmoothing;
     private PlayerCollision collision;
 
     void Start() {
         collision = GetComponent<PlayerCollision>();
-
-        //gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        //maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        //minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
     }
 
     void Update() {
-        /*
-        if (collision.collisionInfo.above || collision.collisionInfo.below) {
-            velocity.y = 0;
-        }
-        */
-
         if (collision.enabled) {
             Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-            /*
-            if (Input.GetKeyDown(KeyCode.Space) && collision.collisionInfo.below) {
-                velocity.y = maxJumpVelocity;
-            }
-            if (Input.GetKeyUp(KeyCode.Space)) {
-                if (velocity.y > minJumpVelocity)
-                    velocity.y = minJumpVelocity;
-            }
-            */
 
             float targetVelX = input.x * hMoveSpeed;
             float targetVelY = input.y * vMoveSpeed;
             velocity.x = Mathf.SmoothDamp(velocity.x, targetVelX, ref velocityXSmoothing, movementSmoothing);
             velocity.y = Mathf.SmoothDamp(velocity.y, targetVelY, ref velocityYSmoothing, movementSmoothing);
-            //velocity.y += gravity * Time.deltaTime;
 
             collision.Move(velocity * Time.deltaTime, input);
         }
         else {
             velocity.x = 0;
-            //velocity.y += gravity * Time.deltaTime;
             velocity.y = 0;
             collision.Move(velocity * Time.deltaTime, new Vector2(0, 0));
         }
