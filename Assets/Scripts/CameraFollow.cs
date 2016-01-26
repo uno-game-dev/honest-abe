2109dfa07c0ef50187d2565017e35fa92620fa73
Parker Sprouse <psprouse@uno.edu>
2016-01-26 01:02:43 -0600

    Cleaned out the directory and created the initial iteration of the camera script. The final iteration will lock the camera from moving above or below the bounds of the level, but I do not have a level to work with to implement that yet

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float smoothTime = 0.15f;
+
+    private Transform playerTransform;
+    private float smoothVelX, smoothVelY;
+
+    void Start() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void LateUpdate() {
+        Vector3 pos = transform.position;
+
+        pos.x = Mathf.SmoothDamp(transform.position.x, playerTransform.position.x, ref smoothVelX, smoothTime);
+        pos.y = Mathf.SmoothDamp(transform.position.y, playerTransform.position.y, ref smoothVelY, smoothTime);
+
+        transform.position = pos;
+    }
+
+}
