470de5cdc8dc0b092192b7f0831541a254eb6c1c
Ted Mader <tlmader@live.com>
2016-01-27 16:20:32 -0600

    Update TerrainSpawner.cs

--- Assets/Scripts/TerrainSpawner.cs
+++ Assets/Scripts/TerrainSpawner.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class TerrainSpawner : MonoBehaviour
-{
+public class TerrainSpawner : MonoBehaviour {
+
 	public GameObject terrain;
 	public float startSpawnPosition = 8f;
 	public int spawnYPos = 0;
@@ -12,24 +11,23 @@ public class TerrainSpawner : MonoBehaviour
 	bool canSpawn = true;
 
 	// Use this for initialization
-	void Start()
-	{
+	void Start() {
+
 		lastPosition = startSpawnPosition;
 		cam = GameObject.Find("Main Camera");
 	}
 	
 	// Update is called once per frame
-	void Update()
-	{
-		if (cam.transform.position.x >= lastPosition - 16 && canSpawn)
-		{
+	void Update() {
+
+		if (cam.transform.position.x >= lastPosition - 16 && canSpawn) {
 			canSpawn = false;
 			SpawnTerrain();
 		}
 	}
 
-	void SpawnTerrain()
-	{
+	void SpawnTerrain() {
+
 		Instantiate(terrain, new Vector3(lastPosition, spawnYPos, 0), Quaternion.Euler(0, 0, 0));
 		lastPosition += startSpawnPosition;
 
