5d7a29c4ebc62fbdab5ce173e54167d5eb09f47c
parkersprouse <psprouse@uno.edu>
2016-01-27 15:15:24 -0600

    Locked camera within a vertical bounds

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,14 @@ public class CameraFollow : MonoBehaviour {
     public float smoothTime = 0.15f;
 
     private Transform playerTransform;
+    private BoxCollider2D levelBounds;
+    private Camera cam;
     private float smoothVelX, smoothVelY;
 
     void Start() {
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        levelBounds = GameObject.Find("_Manager").GetComponent<BoxCollider2D>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     void LateUpdate() {
@@ -17,6 +21,20 @@ public class CameraFollow : MonoBehaviour {
 
         pos.x = Mathf.SmoothDamp(transform.position.x, playerTransform.position.x, ref smoothVelX, smoothTime);
         pos.y = Mathf.SmoothDamp(transform.position.y, playerTransform.position.y, ref smoothVelY, smoothTime);
+
+        // Getting camera size
+        //float height = 2f * cam.orthographicSize;
+        //float width = height * cam.aspect;
+
+        if (pos.y + cam.orthographicSize > levelBounds.transform.position.y + (levelBounds.size.y / 2))
+        {
+            pos.y = (levelBounds.transform.position.y + (levelBounds.size.y / 2) - cam.orthographicSize);
+        }
+
+        if (pos.y - cam.orthographicSize < levelBounds.transform.position.y - (levelBounds.size.y / 2))
+        {
+            pos.y = (levelBounds.transform.position.y - (levelBounds.size.y / 2) + cam.orthographicSize);
+        }
 
         transform.position = pos;
     }
