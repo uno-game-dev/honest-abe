f45a619ce7251a55b96f246032c60a8adc1a64fe
Ted Mader <tlmader@live.com>
2016-02-01 15:50:07 -0600

    Add ObjectDestroyer script

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroyer : MonoBehaviour {
+
+	bool seen = false;
+
+	// Use this for initialization
+	void Start() {
+	}
+
+	// Update is called once per frame
+
+	void Update() {
+
+		if (GetComponent<Renderer>().isVisible) {
+			seen = true;
+		}
+
+		if (seen && !GetComponent<Renderer>().isVisible)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
