d92bd53094fbd9cf69f7e0ca4472ff239934b5da
parkersprouse <psprouse@uno.edu>
2016-02-10 16:19:55 -0600

    First iteration of the item system

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public int increaseAmount;
+
+    void Start()
+    {
+        increaseAmount = 20;
+    }
+
+    public enum ItemType
+    {
+        HEALTH
+    }
+
+    public ItemType type;
+
+    public void OnCollision()
+    {
+        switch (type)
+        {
+            case ItemType.HEALTH:
+                Debug.Log("I am health");
+                Destroy(gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(BaseCollision))]
-public class PlayerMotor : MonoBehaviour {
+public class PlayerMotor : MonoBehaviour
+{
 
     public float hMoveSpeed = 8, vMoveSpeed = 6;
     public float movementSmoothing = .115f;
@@ -9,17 +10,27 @@ public class PlayerMotor : MonoBehaviour {
     private Vector3 velocity;
     private float velocityXSmoothing, velocityYSmoothing;
     private BaseCollision collision;
+    private Health playerHealth;
 
-    void Start() {
+    void Start()
+    {
         collision = GetComponent<BaseCollision>();
+        collision.OnCollision += OnCollision;
+
+        playerHealth = GetComponent<Health>();
     }
 
-    void Update() {
+    void Update()
+    {
 
         // If the game hasn't officially started yet, don't do any update calls
         if (!UIManager.updateActive) return;
 
-        if (collision.enabled) {
+        // Else run the update code
+        if (collision.enabled)
+        {
+            float delta = Time.deltaTime;
+
             Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
             float targetVelX = input.x * hMoveSpeed;
@@ -28,13 +39,30 @@ public class PlayerMotor : MonoBehaviour {
             velocity.x = Mathf.SmoothDamp(velocity.x, targetVelX, ref velocityXSmoothing, movementSmoothing);
             velocity.y = Mathf.SmoothDamp(velocity.y, targetVelY, ref velocityYSmoothing, movementSmoothing);
 
-            collision.Move(velocity * Time.deltaTime);
+            collision.Move(velocity * delta);
         }
-        else {
+        else
+        {
             velocity.x = 0;
             velocity.y = 0;
             collision.Move(velocity * Time.deltaTime);
         }
     }
+
+    private void OnCollision(RaycastHit2D hit)
+    {
+        Debug.Log(hit.collider);
+        if (hit.collider.tag == "Item")
+        {
+            Item item = hit.transform.gameObject.GetComponent<Item>();
+            if (item.type == Item.ItemType.HEALTH)
+            {
+                //playerHealth.Increase(item.increaseAmount);
+            }
+            item.OnCollision();
+        }
+    }
+
+
 
 }
