d5d1ae2ad70d650545ae2f281f6f01de1f584c88
Maurice Robert <mcosterobert@gmail.com>
2016-02-11 22:15:25 -0600

    Add directional control as Toggleable variable in inspector

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class ScrollingBackground : MonoBehaviour {
 
 	public float scrollSpeed;
+	public Direction scrollDirection = Direction.Right;
+
 	private Vector2 savedOffset;
 	new private Renderer renderer;
-
+	private float directionScalar = 0f;
+	[HideInInspector]public enum Direction {None, Left, Right };
 	private CameraFollow cameraFollow;
 
 
@@ -14,13 +17,15 @@ public class ScrollingBackground : MonoBehaviour {
 		renderer = this.GetComponent<Renderer>();
 		savedOffset = renderer.material.mainTextureOffset;
 		cameraFollow = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<CameraFollow> ();
+		if( scrollDirection != Direction.None)
+			directionScalar = scrollDirection == Direction.Right ? 1f : -1f; 
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if ( cameraFollow.velocity.x > 0f && cameraFollow.velocity.magnitude > 0f) {
-			float x = renderer.material.mainTextureOffset.x + (scrollSpeed * (cameraFollow.velocity.magnitude/-1000f));
+			float x = renderer.material.mainTextureOffset.x + (scrollSpeed * (cameraFollow.velocity.magnitude * Time.deltaTime) * directionScalar);
 			Vector2 offset = new Vector2 (x, savedOffset.y);
 			renderer.material.mainTextureOffset = offset;
 			Debug.Log (cameraFollow.velocity.magnitude);
