8c43067379e0f599adabef4814637681dbbe20b3
Ted Mader <tlmader@live.com>
2016-02-15 17:30:15 -0600

    update Terrain and TerrainSpawner prefabs

--- Assets/Scripts/TerrainSpawner.cs
+++ Assets/Scripts/TerrainSpawner.cs
@@ -9,10 +9,10 @@ public class TerrainSpawner : MonoBehaviour {
 	public float startSpawnPosition = 8f;
 	public int spawnYPos = 0;
 	public int spawnZPos = 10;
+	public int propDensity = 3;
+	public int enemyDensity = 3;
 
 	private GameObject cam;
-	private GameObject gm;
-	private GameObject lastTerrain;
 	private System.Random rnd;
 	private bool canSpawn = true;
 	private float lastPosition;
@@ -22,7 +22,6 @@ public class TerrainSpawner : MonoBehaviour {
 		rnd = new System.Random();
 		lastPosition = startSpawnPosition;
 		cam = GameObject.Find("Main Camera");
-		gm = GameObject.Find("GameManager");
 	}
 	
 	// Update is called once per frame
@@ -40,28 +39,34 @@ public class TerrainSpawner : MonoBehaviour {
 
 	private void SpawnTerrain() {
 
-		lastTerrain = Instantiate(terrain, new Vector3(lastPosition, spawnYPos, spawnZPos), Quaternion.Euler(0, 0, 0)) as GameObject;
+		Instantiate(terrain, new Vector3(lastPosition, spawnYPos, spawnZPos), Quaternion.Euler(0, 0, 0));
 	}
 
 	private void SpawnProp() {
 
-		int r = rnd.Next(props.Count);
-		Instantiate(props[r], getRandomPos(), Quaternion.Euler(0, 0, 0));
+		for (int i = 1; i <= propDensity; i++)
+		{
+			int r = rnd.Next(props.Count);
+			Instantiate(props[r], getRandomPos(), Quaternion.Euler(0, 0, 0));
+		}
 	}
 
 	private void SpawnEnemy() {
-
-		int r = rnd.Next(enemies.Count);
-		Instantiate(enemies[r], getRandomPos(), Quaternion.Euler(0, 0, 0));
+		
+		for (int i = 1; i <= enemyDensity; i++)
+		{
+			int r = rnd.Next(enemies.Count);
+			Instantiate(enemies[r], getRandomPos(), Quaternion.Euler(0, 0, 0));
+		}
 	}
 
 	private Vector3 getRandomPos() {
 		RectTransform area = (RectTransform)terrain.transform;
 		double width = area.rect.width;
-		double height = area.rect.height;
+		double height = area.rect.height * 0.55;
 
 		float x = (float)((width * rnd.NextDouble()) - (width / 2) + lastPosition);
-		float y = (float)((height * rnd.NextDouble()) - (height / 2));
+		float y = (float)((height * rnd.NextDouble()) - (height / 2) - (width * 0.3));
 
 		return new Vector3(x, y, 1);
 	}
