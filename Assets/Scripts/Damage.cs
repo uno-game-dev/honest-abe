2fa21970d1ecaaac7d80affdd507eb165cd92d28
Edward Thomas Garcia <etgarcia@uno.edu>
2016-02-15 13:30:45 -0600

    Added Blood!

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -4,11 +4,16 @@ using System;
 
 public class Damage : MonoBehaviour
 {
-
     public float damageAmount;
     public float damageRate = 1f;
+    public GameObject bloodSplatter;
+    public GameObject bloodFountain;
+
     private Health health;
     private BaseCollision collision;
+    private bool canAddBlood;
+    private float bloodTimer;
+    private float bloodRate = 0.2f;
 
     void Start()
     {
@@ -19,6 +24,12 @@ public class Damage : MonoBehaviour
     void Update()
     {
         collision.Tick();
+
+        if (!canAddBlood)
+            if (bloodTimer > bloodRate)
+                canAddBlood = transform;
+            else
+                bloodTimer += Time.deltaTime;
     }
 
     public void ExecuteDamage(GameObject toObject)
@@ -35,8 +46,37 @@ public class Damage : MonoBehaviour
         {
             damageAmount = hit.transform.GetComponentInParent<Attack>().GetDamageAmount();
             ExecuteDamage(gameObject);
+            AddBlood(hit);
             Debug.Log("Hit! " + gameObject.name);
         }
     }
 
+    private void AddBlood(RaycastHit2D hit)
+    {
+        if (!canAddBlood)
+            return;
+
+        bloodTimer = 0;
+        canAddBlood = false;
+
+        Weapon weapon = hit.transform.GetComponentInParent<Attack>().weapon;
+        if (weapon.attackType == Weapon.AttackType.Swing)
+        {
+            if (bloodFountain)
+            {
+                GameObject blood = Instantiate(bloodFountain);
+                blood.transform.localPosition = hit.point;
+                Destroy(blood, 10);
+            }
+        }
+        else
+        {
+            if (bloodSplatter)
+            {
+                GameObject blood = Instantiate(bloodSplatter);
+                blood.transform.localPosition = hit.point;
+                Destroy(blood, 10);
+            }
+        }
+    }
 }
