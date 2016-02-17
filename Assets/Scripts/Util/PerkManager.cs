511dcc74ea7e953b5e8b3dafa4543efb961fcf27
parkersprouse <psprouse@uno.edu>
2016-02-17 16:59:31 -0600

    Syncing to remote repo

--- Assets/Scripts/Util/PerkManager.cs
+++ Assets/Scripts/Util/PerkManager.cs
@@ -7,12 +7,12 @@ public class PerkManager : MonoBehaviour {
     public List<Perk> perkList = new List<Perk>();
 
     void Start () {
-        PreferenceManager.UpdatePerkStatus();
-
         GameObject[] perksInLevel = GameObject.FindGameObjectsWithTag("Perk");
         for (int i = 0; i < perksInLevel.Length; i++)
         {
-            perkList.Add(perksInLevel[i].GetComponent<Perk>());
+            Perk p = perksInLevel[i].GetComponent<Perk>();
+            p.CheckStatus();
+            perkList.Add(p);
         }
 	}
 	
