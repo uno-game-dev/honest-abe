98491d532e309daf7771023351c2e7992c8c5400
Maurice Coste Robert III <mrobert@uno.edu>
2016-02-22 17:49:20 -0600

    Adding Audio Manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,14 +2,45 @@
 using System.Collections;
 
 public class AudioManager : MonoBehaviour {
+    
+    public AudioClip introMusic;
+    public AudioClip playMusic;
+
+    private static AudioSource CameraSource = null;
 
 	// Use this for initialization
 	void Start () {
-	
+        CameraSource = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if(UIManager.updateActive)
+        {
+            PlayGameMusic();
+        }
+        else
+        {
+            PlayIntroMusic();
+        }
 	}
+
+    void PlayIntroMusic()
+    {
+        if (CameraSource.clip != introMusic)
+        {
+            CameraSource.clip = introMusic;
+            CameraSource.Play();
+        }
+    }
+
+    void PlayGameMusic()
+    {
+        if (CameraSource.clip != playMusic)
+        {
+            CameraSource.clip = playMusic;
+            CameraSource.Play();
+        }
+    }
 }
