1a3c6408909bf2779ede13e20a11a38ec2f7762c
Edward Thomas Garcia <etgarci1@uno.edu>
2016-02-24 13:19:50 -0600

    Added YtoZ to gameobjects.

--- Assets/Scripts/YtoZ.cs
+++ Assets/Scripts/YtoZ.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class YtoZ : MonoBehaviour {
+    public float offsetFromOrigin = 2f;
+    public float maxZ = 45;
     public float yToZFactor = 2f;
 
     private void Update()
@@ -12,7 +14,8 @@ public class YtoZ : MonoBehaviour {
     private void ChangeZBasedOnY()
     {
         Vector3 localPosition = transform.localPosition;
-        localPosition.z = localPosition.y * yToZFactor;
+        localPosition.z = -offsetFromOrigin + localPosition.y * yToZFactor;
+        localPosition.z = Mathf.Clamp(localPosition.z, -maxZ, 0);
         transform.localPosition = localPosition;
     }
 }
