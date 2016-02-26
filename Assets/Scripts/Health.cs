4246c8a1018fd09f5301d9b1c46cc3972fac0067
Ted Mader <tlmader@live.com>
2016-02-25 21:23:51 -0600

    Update scripts for execution fix

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -27,10 +27,16 @@ public class Health : MonoBehaviour {
 			//If the hit would kill the gameObject
 			if ((tempHealth -= damage) <= 0) {
 				health = 0;
-				if(gameObject.tag == "Boss"){
+                // Execution Check
+                if (gameObject.tag != "Player" && GlobalSettings.performingHeavyAttack)
+                {
+                    GlobalSettings.executionPerformed = true;
+                    ShowExecution();
+                }
+                if (gameObject.tag == "Boss"){
 					GameManager.win = true; // TESTING for Win game in alpha
-				}	
-				UpdateHealth ();
+                }
+                UpdateHealth ();
 				Destroy (gameObject);
 			} else {
 				health -= damage;
@@ -43,6 +49,21 @@ public class Health : MonoBehaviour {
 		if (gameObject.tag == "Enemy") {
 			Debug.Log ("Enemy health is: " + health);
 		}
-	}
+    }
+
+    public void ShowExecution()
+    {
+        GameObject number = new GameObject();
+        number.name = "Execution";
+        TextMesh tm = number.AddComponent<TextMesh>();
+        tm.text = "R.I.P.";
+        tm.fontSize = 24;
+        tm.color = Color.red;
+        tm.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        tm.transform.position = transform.position;
+        FloatUpAndDestroy f = number.AddComponent<FloatUpAndDestroy>();
+        f.floatGravityMultiplier = 0.5f;
+        f.floatVelocity = 2;
+    }
 
 }
