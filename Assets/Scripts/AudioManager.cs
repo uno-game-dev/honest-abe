a1351ba664de41890965346e2aafd93d8ba5cafc
Maurice Robert <mcosterobert@gmail.com>
2016-02-27 11:10:18 -0600

    Updated AudioManager and Main Camera Prefab, Updated AudioManager script.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,16 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour {
-    
+
+    public static AudioManager instance = null;
     public AudioClip introMusic;
     public AudioClip playMusic;
 
-    private static AudioSource CameraSource = null;
+    public AudioClip[] AttackSounds = new AudioClip[2];
+    public AudioClip[] DamageSounds = new AudioClip[2];
+	public AudioClip[] GenericSounds = new AudioClip[4];
 
+    public Dictionary<string, AudioClip> Sounds = new Dictionary<string, AudioClip>(2);
+
+    private static AudioSource CameraSource = null;
 	// Use this for initialization
 	void Start () {
+        if (instance == null)
+            instance = this;
         CameraSource = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        initSoundLib();
 	}
 	
 	// Update is called once per frame
@@ -42,4 +52,31 @@ public class AudioManager : MonoBehaviour {
             CameraSource.Play();
         }
     }
+
+    private void initSoundLib()
+    {
+        Sounds["Swing"] = AttackSounds[0];
+   
+    }
+
+    public void PlaySound( string name)
+    {
+        if (Sounds.ContainsKey(name))
+            AudioSource.PlayClipAtPoint(Sounds[name], CameraSource.transform.position, 0.7f);
+    }
+
+    public void PlayDamageSound( int index = 0)
+    {
+        AudioSource.PlayClipAtPoint(DamageSounds[index], CameraSource.transform.position, 0.7f);
+    }
+
+    public void PlayAttackSound(int index = 0)
+    {
+        AudioSource.PlayClipAtPoint(AttackSounds[index], CameraSource.transform.position, 2f);
+    }
+
+	public void PlayGenericSound(int index = 0)
+	{
+		AudioSource.PlayClipAtPoint(GenericSounds[index], CameraSource.transform.position, 2f);
+	}
 }
