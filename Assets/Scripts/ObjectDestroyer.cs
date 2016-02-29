e02bb1992605f48d3ac4fbe2924ed3f0f1b0eaf0
Ted Mader <tlmader@live.com>
2016-02-29 00:15:45 -0600

    Clean up code

--- Assets/Scripts/ObjectDestroyer.cs
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -1,17 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ObjectDestroyer : MonoBehaviour
 {
-
     bool seen = false;
 
-    // Use this for initialization
     void Start()
     {
     }
-
-    // Update is called once per frame
 
     void Update()
     {
