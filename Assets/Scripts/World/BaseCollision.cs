2aa82418c4e59d8760c024fbdbf2f68eba49ae7d
Parker Sprouse <psprouse@uno.edu>
2016-03-01 01:40:37 -0600

    Small cleanups to BaseCollision, started commenting whole script

--- Assets/Scripts/World/BaseCollision.cs
+++ Assets/Scripts/World/BaseCollision.cs
@@ -3,42 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+
+/**
+ * BaseCollision is the class that all objects that will need to trigger a collision with other objects will need.
+ * Objects that will only receive collisions and not do the triggering will NOT need BaseCollision.
+ * In order for an object to use BaseCollision, it must have a Collider2D - any 2D collider will work.
+ **/ 
+
 [RequireComponent(typeof(Collider2D))]
 public class BaseCollision : MonoBehaviour
 {
 
+    /*
+     * The state of the current collision
+     * Null - no collision
+     * CollisionStart - the first frame the collision happened
+     * CollisionStay - each frame the collision is happening after the start
+     * CollisionEnd - the final frame the collision happened
+     */ 
     public enum State { Null, CollisionStart, CollisionStay, CollisionEnd }
 
+    // The velocity of the parent object
     private Vector2 velocity;
     public Vector2 Velocity
     {
         get { return velocity; }
     }
 
+    // The number of horizontal rays that the object will use to detect collisions
     public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
-    public LayerMask collisionLayer;
-    public CollisionInfo collisionInfo;
-    public int numberOfCollisions;
-    [HideInInspector]
-    public BoxCollider2D _collider;
 
+    // The number of vertical rays that the object will use to detect collisions
+    public int verticalRayCount = 4;
+
+    // The LayerMask that will be used to check for collisions against
+    public LayerMask collisionLayer;
+
+    // Our instance of an internal struct used to get if a collision is happening on any of the cardinal directions of the object
+    public CollisionInfo collisionInfo;
+
+    // The total number of collisions that this object is currently taking part in
+    public int numberOfCollisions;
+
+    /*
+     * The delegates used to "fill in the blanks" for what events will be fired at different stages of the collision
+     * OnCollisionStay - what happens each frame of the collision
+     * OnCollisionEnter - what happens the first frame the collision happens
+     * OnCollisionExit - what happens the final frame the collision happens
+     */ 
     public delegate void CollisionHandler(Collider2D collider);
     public event CollisionHandler OnCollisionStay = delegate { };
     public event CollisionHandler OnCollisionEnter = delegate { };
     public event CollisionHandler OnCollisionExit = delegate { };
 
+    /*
+     * Horizontal Ray Spacing and Vertical Ray Spacing determines how far apart each ray of the raycasts are placed
+     * They are not manually set and are instead calculated based on how many horizontal / vertical rays there are
+     * This ensures that collisions are being checked for evenly across the object, minimizing the chance of missing a collision
+     */ 
     private float horizontalRaySpacing, verticalRaySpacing;
+
+    // A reference to the Collider2D on the object
+    private Collider2D _collider;
+
+    /*
+     * The spot on the object that we will start casting rays from
+     * This is primarily for moving objects so that we only check for collisions in the direction we're moving
+     * This makes the checks more efficient since we don't have to worry about triggering collisions in the direction
+     *     we are not currently moving in
+     */
     private RaycastOrigins raycastOrigins;
+
+    // A list of all of the current collisions with the object and the state those collisions are in
     public Dictionary<Collider2D, State> _currentCollisions = new Dictionary<Collider2D, State>();
 
+    // Essentially, the size of the rays being cast
     private const float skinWidth = .015f;
 
+    // Struct associated with raycastOrigins
     private struct RaycastOrigins
     {
         public Vector2 topLeft, topRight, bottomLeft, bottomRight;
     }
 
+    // Struct associated with collisionInfo
     public struct CollisionInfo
     {
         public bool above, below, left, right;
@@ -51,32 +99,38 @@ public class BaseCollision : MonoBehaviour
 
     void Awake()
     {
-        _collider = GetComponent<BoxCollider2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
     void Start()
     {
+        // On start, calculate the spacing between the rays based on how many rays are being cast
         CalculateRaySpacing();
     }
 
     void OnDrawGizmos()
     {
+        // Draw a circle around the object in the editor for debugging
         Gizmos.DrawWireSphere(transform.position, 1);
     }
 
     void Update()
     {
-        Vector2 pointA = new Vector2(transform.position.x, transform.position.y) + _collider.offset - _collider.size / 2;
-        Vector2 pointB = new Vector2(transform.position.x, transform.position.y) + _collider.offset + _collider.size / 2;
-
+        // Get an array of all of the objects currently being collided with
+        Vector2 pointA = new Vector2(transform.position.x, transform.position.y) + _collider.offset - (Vector2)_collider.bounds.size / 2;
+        Vector2 pointB = new Vector2(transform.position.x, transform.position.y) + _collider.offset + (Vector2)_collider.bounds.size / 2;
         Collider2D[] currentCollisions = Physics2D.OverlapAreaAll(pointA, pointB, collisionLayer);
+
+        // Go through each collider that we're colliding with and run the AddCollision method using each collider
         foreach (Collider2D collider in currentCollisions)
             AddCollision(collider);
 
+        // Go through each collider that we're colliding with and run the OnCollisionStay method using each collider
         foreach (Collider2D collider in _currentCollisions.Keys)
             if (collider)
                 OnCollisionStay(collider);
 
+        // Go through each collider that we're colliding with and run the RemoveCollision method using each collider
         for (int i = _currentCollisions.Keys.Count - 1; i >= 0; i--)
         {
             Collider2D collider = _currentCollisions.Keys.ElementAt(i);
@@ -88,6 +142,7 @@ public class BaseCollision : MonoBehaviour
 
     void OnDisable()
     {
+        // If we disable the BaseCollision component, remove all current collisions
         for (int i = _currentCollisions.Keys.Count - 1; i >= 0; i--)
             RemoveCollision(_currentCollisions.Keys.ElementAt(i));
     }
@@ -198,6 +253,7 @@ public class BaseCollision : MonoBehaviour
     private void RemoveCollision(Collider2D collider)
     {
         if (collider == _collider) return;
+
         if (_currentCollisions.ContainsKey(collider))
         {
             _currentCollisions[collider] = State.CollisionEnd;
