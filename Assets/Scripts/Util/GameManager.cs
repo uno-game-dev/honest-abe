e00901952b160c04302bcb1a9c35d72c5ec3dbaa
Ted Mader <tlmader@live.com>
2016-03-07 16:02:16 -0600

    Add LevelManager script, update Boss, Gamemanager scripts

--- Assets/Scripts/Util/GameManager.cs
+++ Assets/Scripts/Util/GameManager.cs
@@ -6,14 +6,17 @@ public class GameManager : MonoBehaviour
     public bool lose;
     public bool win;
     private CameraFollow _cameraFollow;
+	private LevelManager _levelManager;
 
-    void Start()
+	void Start()
     {
+
         perkChosen = false;
 		lose = false;
         win = false;
 		
         _cameraFollow = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
+		_levelManager = GetComponent<LevelManager>();
         if (!perkChosen)
 			_cameraFollow.lockRightEdge = true;
     }
@@ -29,9 +32,11 @@ public class GameManager : MonoBehaviour
 		//Checks if the boss health is 0 -- for alpha
 		if (win)
 		{
-			UIManager.displayWin = true;
+			win = false;
+			// UIManager.displayWin = true;
 			PerkManager.UpdatePerkStatus(GlobalSettings.axe_dtVampirism_name, 1);
-		}
+			_levelManager.loadNextLevel();
+        }
 	}
 
 	public void CheckLost()
--- Assets/Scripts/Util/LevelManager.cs
+++ Assets/Scripts/Util/LevelManager.cs
@@ -1,18 +1,39 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 // The LevelManager class handles level transitions and selection.
 public class LevelManager : MonoBehaviour {
 
-	public int startingLevel;
+	public int startingScene;
+	public int currentScene;
+
+	private static LevelManager _instance;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake()
+	{
+		if (_instance == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			_instance = this;
+			currentScene = startingScene;
+			SceneManager.LoadScene(currentScene);
+		}
+		else if (_instance != this)
+		{
+			Destroy(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+	}
+
+	public void loadNextLevel()
+	{
+		currentScene++;
+		SceneManager.LoadScene(currentScene);
 	}
 }
