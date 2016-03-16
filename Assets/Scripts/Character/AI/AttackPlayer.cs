295dca4826de6041245630a5e21d3a3194b7b58d
Rachel Farrell <rfarrell@uno.edu>
2016-03-16 11:28:49 -0500

    ...

--- Assets/Scripts/Character/AI/AttackPlayer.cs
+++ Assets/Scripts/Character/AI/AttackPlayer.cs
@@ -9,17 +9,10 @@ public class AttackPlayer : ActionNode {
 	public override Status Update () {
 		if (Random.value > 0.35) {
 			self.GetComponent<Attack> ().LightAttack ();
-			spin (0.5f);
 		} else {
 			self.GetComponent<Attack>().HeavyAttack();
-			spin (2f);
 		}
 
 		return Status.Success;
 	}
-
-	public IEnumerator spin(float n){
-		yield return new WaitForSeconds (n);
-	}
-
 }
--- Assets/Scripts/Character/AI/ChasePlayer.cs
+++ Assets/Scripts/Character/AI/ChasePlayer.cs
@@ -7,12 +7,6 @@ public class ChasePlayer : ActionNode {
 	public override Status Update () {
 
 		self.GetComponent<EnemyFollow> ().targetType = EnemyFollow.TargetType.Player;
-		//spin (0.5f);
-
 		return Status.Running;
-	}
-
-	public IEnumerator spin(float n){
-		yield return new WaitForSeconds (n);
 	}
 }
