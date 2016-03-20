9a115eda122c93d4f3147c9f1e96510c1ae87239
ETdoFresh <ETdoFresh@gmail.com>
2016-03-19 17:18:01 -0700

    Created a Model Loader for Loading Models in case I make a change to them. Started keyframing falling animation

---
+++ Assets/Scripts/Character/ModelLoader.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelLoader : MonoBehaviour
+{
+    public GameObject model;
+    public Vector3 localPosition = new Vector3(0, -0.5f, 0);
+    public Vector3 localRotation = new Vector3(0, 145, 0);
+    public Vector3 localScale = new Vector3(3, 3, 3);
+    public GameObject loadedModel;
+    public RuntimeAnimatorController runtimeAnimationController;
+    private Animator _animator;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.avatar = null;
+    }
+
+    void OnEnable()
+    {
+        DestroyLoadedModel(true);
+
+        if (!model)
+            return;
+
+        loadedModel = Instantiate(model);
+        loadedModel.transform.SetParent(transform);
+        loadedModel.name = model.name;
+        loadedModel.transform.localPosition = localPosition;
+        loadedModel.transform.localEulerAngles = localRotation;
+        loadedModel.transform.localScale = localScale;
+
+        Animator modelAnimator = loadedModel.GetComponent<Animator>();
+        if (_animator && modelAnimator)
+            _animator.avatar = modelAnimator.avatar;
+    }
+
+    void OnDisable()
+    {
+        DestroyLoadedModel();
+    }
+
+    void DestroyLoadedModel(bool destroyImmediately = false)
+    {
+        if (loadedModel)
+        {
+            if (destroyImmediately)
+                DestroyImmediate(loadedModel);
+            else
+                Destroy(loadedModel);
+        }
+
+        loadedModel = null;
+        _animator.avatar = null;
+    }
+}
