2915312dd2f3b99ba18b18f5dda7a2e1a71afa3f
Rachel Farrell <rfarrell@uno.edu>
2016-04-01 14:54:07 -0500

    Implement basic Bushwhacker AI. Bushwhackers run up to the player, knock him down, attack him while he's down, then run away. For now they just run towards an obstacle and stop.

---
+++ Assets/Scripts/Character/AI/RunAndHide.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourMachine;
+
+public class RunAndHide : ActionNode {
+
+	private GameObject[] obstacles; 
+	private EnemyFollow enemyFollow;
+
+	public override void Start () {
+		// Find position of nearest obstacle, or bush whatever
+		obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+		enemyFollow = self.GetComponent<EnemyFollow> ();
+
+		// Set enemyfollow target to that
+
+		// But for now, just go towards the first obstacle
+		enemyFollow.targetType = EnemyFollow.TargetType.TargetGameObject;
+		enemyFollow.target = obstacles [0];
+	}
+	
+	public override Status Update () {
+		return Status.Success;
+	}
+}
