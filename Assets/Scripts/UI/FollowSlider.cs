300abdcbe6dabca2bc198b82d5af9c57ef5569f4
ETdoFresh <ETdoFresh@gmail.com>
2016-04-01 19:11:58 -0500

    Blood Drips from health bar when DT is greater than health (and DT is decreasing).

---
+++ Assets/Scripts/UI/FollowSlider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowSlider : MonoBehaviour
+{
+    public RectTransform slider;
+    public Vector3 sliderPos;
+    public Vector3 sliderPosToWorld;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderPos = slider.position;
+        sliderPosToWorld = Camera.main.ScreenToWorldPoint(sliderPos);
+        Vector2 pos = sliderPosToWorld + offset;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Util/BloodDripper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodDripper : MonoBehaviour
+{
+    private new ParticleSystem particleSystem;
+    public GameObject trackedGameObject;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (trackedGameObject.activeSelf && particleSystem.isStopped)
+            particleSystem.Play();
+        else if (!trackedGameObject.activeSelf && particleSystem.isPlaying)
+            particleSystem.Stop();
+    }
+}
