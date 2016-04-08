e0553fad547db694d2c0c9eeeb3e2ca313c017d7
Rachel Farrell <rfarrell@uno.edu>
2016-04-08 16:55:40 -0500

    Make bushwhackers move more smoothly

--- Assets/Scripts/Character/AI/RunAwayFromPlayer.cs
+++ Assets/Scripts/Character/AI/RunAwayFromPlayer.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 using BehaviourMachine;
 
-public class RunToDistance : ActionNode {
+public class RunAwayFromPlayer : ActionNode {
 
 	private BaseCollision baseCollision;
 	private GameObject player;
 	private Vector3 playerPosition;
 	private Vector3 selfPosition;
+	private Movement movement;
+	private EnemyFollow enemyFollow;
 
 	public override void Start () {
 		player = GameObject.Find ("Player");
 		baseCollision = self.GetComponent<BaseCollision> ();
+		movement = self.GetComponent<Movement> ();
+		enemyFollow = self.GetComponent<EnemyFollow> ();
+		enemyFollow.targetType = EnemyFollow.TargetType.Null;
 	}
 
 	public override Status Update () {
@@ -19,7 +24,7 @@ public class RunToDistance : ActionNode {
 		playerPosition = player.transform.position;
 		selfPosition = self.transform.position;
 		Vector3 vectorToPlayer = playerPosition - selfPosition;
-		baseCollision.Move(Vector3.ClampMagnitude(vectorToPlayer * -1, 0.14f));
+		baseCollision.Move(Time.deltaTime * -vectorToPlayer.normalized * movement.horizontalMovementSpeed);
 		return Status.Success;
 	}
 }
