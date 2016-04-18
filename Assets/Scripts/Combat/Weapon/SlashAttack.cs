a07e1692eff8403bb51fdce07ed8a09dd441c038
Maurice Robert <mcosterobert@gmail.com>
2016-04-17 20:32:50 -0500

    Add Saber Prefab, SlashAttack Script and AttackType to project

---
+++ Assets/Scripts/Combat/Weapon/SlashAttack.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class SlashAttack : BaseAttack
+{
+	public enum SwingChain { First, Second, Third }
+	public SwingChain chain;
+
+	protected override void PrepareToLightAttack()
+	{
+		base.PrepareToLightAttack();
+		if (chain == SwingChain.First)
+		{
+			animator.Play("Light Attack Saber Right");
+			chain = SwingChain.Second;
+		}
+		else
+		{
+			animator.Play("Light Attack Saber Left");
+			chain = SwingChain.First;
+		}
+	}
+
+	protected override void PrepareToHeavyAttack()
+	{
+		base.PrepareToHeavyAttack();
+		animator.Play("Heavy Attack Saber Stab");
+
+		chain = SwingChain.First;
+	}
+
+	protected override void PerformLightAttack()
+	{
+		bool hand = chain == SwingChain.Second;
+		if (swipe) swipe.Activate(hand);
+		base.PerformLightAttack();
+	}
+
+	protected override void PerformHeavyAttack()
+	{
+		//animator.Play("Heavy Attack Saber Stab");
+		bool hand = chain == SwingChain.Second;
+		if (swipe) swipe.Activate(hand);
+		base.PerformHeavyAttack();
+	}
+
+	protected override void BackToIdle()
+	{
+		base.BackToIdle();
+	}
+}
