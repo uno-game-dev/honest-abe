5955417bc0be0145b684b02daf2a26e8abb6e2ef
Parker Sprouse <psprouse@uno.edu>
2016-04-24 01:24:35 -0500

    Created a new script that adjusts the z-axis of the props to create the sense of depth

---
+++ Assets/Scripts/World/PropDepth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PropDepth : MonoBehaviour {
+
+    private Transform _playerTransform, _transform;
+    private Vector3 _underPlayer, _overPlayer;
+
+	void Start () {
+        _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        _transform = GetComponent<Transform>();
+
+        _underPlayer = new Vector3(_transform.position.x, _transform.position.y, -40f);
+        _overPlayer = new Vector3(_transform.position.x, _transform.position.y, 1f);
+    }
+	
+	void Update () {
+        if (_playerTransform.position.y > _transform.position.y)
+            _transform.position = _underPlayer;
+        else
+            _transform.position = _overPlayer;
+	}
+}
