5986364545f2cdfd74cbf43b7efb3070a11227d8
ETdoFresh <ETdoFresh@gmail.com>
2016-04-24 13:04:31 -0500

    Shows locks where inactive/locked Perks would be. Also shows description at the top.

---
+++ Assets/Scripts/Util/LockedPerk.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LockedPerk : MonoBehaviour
+{
+    public bool isLocked;
+    public GameObject lockIcon;
+
+    public void Lock()
+    {
+        isLocked = true;
+        for (int i = transform.childCount - 1; i >= 0; i--)
+            Destroy(transform.GetChild(i).gameObject);
+        GameObject lo = Instantiate(lockIcon);
+        lo.transform.SetParent(transform, false);
+    }
+}
