5cb315761fe587307110812dd3883b2b551fa3ab
Maurice Robert <mcosterobert@gmail.com>
2016-04-24 22:37:37 -0500

    Add Lunge state to Bushwhacker AI. Update Lunge attack to perform attack simultaneously as movement.

--- Assets/Scripts/Character/AI/LungeAtPlayer.cs
+++ Assets/Scripts/Character/AI/LungeAtPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using BehaviourMachine;
 
-public class LungeAtPlayer : ConditionNode {
+public class LungeAtPlayer :ActionNode {
 
 	private Attack attack;
 
@@ -12,13 +12,7 @@ public class LungeAtPlayer : ConditionNode {
 
 	public override Status Update () {
 		// Attack Abe
-		if (Random.value > 0.35) {
-			(self.GetComponent<LungeAttack> ()).Lunge (BaseAttack.Strength.Heavy);
-		} else {
-			(self.GetComponent<LungeAttack> ()).Lunge (BaseAttack.Strength.Light);
-		}
-		if (onSuccess.id != 0)
-			owner.root.SendEvent (onSuccess.id);
+			(self.GetComponent<LungeAttack> ()).Lunge(BaseAttack.Strength.Heavy);
 		return Status.Success;
 	}
 }
