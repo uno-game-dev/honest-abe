6f3c27b2f07bd9a5b659f7fa6ad858a389b71a16
Ted Mader <tlmader@live.com>
2016-04-24 19:34:00 -0500

    Add MissingAxe prefab and DisplayMissingAxe script

---
+++ Assets/Scripts/World/DisplayMissingAxe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayMissingAxe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (Transform child in transform)
+		{
+			if (transform.GetComponent<Perk>().type == PerkManager.activeAxePerk.type)
+				child.gameObject.SetActive(true);
+			else
+				child.gameObject.SetActive(false);
+		}
+	}
+}
