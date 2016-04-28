1ed94b47f2ae04ac3f25df2246b22f52c57ca015
Rachel Farrell <rfarrell@uno.edu>
2016-04-28 14:15:40 -0500

    Make Officer-Boss occasionally back up and let you hit him for a few seconds

---
+++ Assets/Scripts/Character/AI/ChanceToCooldown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourMachine;
+
+public class ChanceToCooldown : ConditionNode {
+
+	private float cooldownChance;
+
+	// Use this for initialization
+	public override void Start () {
+		cooldownChance = blackboard.GetFloatVar ("cooldownChance");
+	}
+
+	// Update is called once per frame
+	public override Status Update () {
+		if (Random.value < cooldownChance) {
+			if (onSuccess.id != 0)
+				owner.root.SendEvent (onSuccess.id);
+			return Status.Success;
+		}
+		return Status.Failure;
+	}
+}
