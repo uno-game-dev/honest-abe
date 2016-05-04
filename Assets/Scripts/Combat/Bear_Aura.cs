c6cc2254b7586885d93779b9c5b9c1a4fbddadd9
Maurice Robert <mcosterobert@gmail.com>
2016-05-03 20:29:32 -0500

    Added timing element to hit, also added knockback direction based on positioning.

--- Assets/Scripts/Combat/Bear_Aura.cs
+++ Assets/Scripts/Combat/Bear_Aura.cs
@@ -3,25 +3,34 @@ using System.Collections;
 
 public class Bear_Aura : MonoBehaviour {
 
-	public float damage = 1;
+	public float damage = 6f;
 	public float stunAmount = 0.2f;
-	public float knockbackAmount = 5f;
+	public float knockbackAmount = 10f;
+	public float damageInterval = 0.2f;
+
+	private float _damageElapsed = 0f;
+	private Stun.Direction _dir = Stun.Direction.Left;
+	private Stun.Power _pow = Stun.Power.Heavy;
+
 
 	 BaseCollision _collision;
 
 	private void Awake()
 	{
 		_collision = GetComponent<BaseCollision>();
+		_damageElapsed = damageInterval;
 	}
 
 	private void OnEnable()
 	{
 		_collision.OnCollisionStay += OnCollision;
+		_collision.OnCollisionExit += Reset;
 	}
 
 	private void OnDisable()
 	{
 		_collision.OnCollisionStay -= OnCollision;
+		_collision.OnCollisionExit -= Reset;
 	}
 
 	void OnCollision( Collider2D collider)
@@ -32,9 +41,30 @@ public class Bear_Aura : MonoBehaviour {
 			if(collider.gameObject.tag == "Player" )
 			{
 				Debug.Log("The Bear collided with the player!");
-				collider.gameObject.GetComponent<Damage>().ExecuteDamage( damage, _collision.GetComponent<Collider2D>());
-				collider.gameObject.GetComponent<Stun>().GetStunned( stunAmount, knockbackAmount, GetComponent<Movement>().direction == Movement.Direction.Right? 1: -1 );
+				if( _damageElapsed > damageInterval )
+				{
+					_damageElapsed =0f;
+					float knockbackDirection = (transform.position - collider.transform.position).normalized.x * -1;
+
+					collider.gameObject.GetComponent<Damage>().ExecuteDamage( damage, _collision.GetComponent<Collider2D>());
+					collider.gameObject.GetComponent<Stun>().GetStunned( 
+						stunAmount, 
+						knockbackAmount, 
+						knockbackDirection//GetComponent<Movement>().direction == Movement.Direction.Right? 1: -1 
+					);
+
+				}
+				else
+				{
+					_damageElapsed += Time.deltaTime;
+				}
+
 			}
 		}
+	}
+
+	void Reset( Collider2D collider)
+	{
+		_damageElapsed = damageInterval;
 	}
 }
